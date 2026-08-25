73e6d2a36e1693fcb36f84bf5a5f6466db79698f
Ridhima Sharma <sharma.ridhima.2310@gmail.com>
2026-08-25 14:16:18 +0530

    Implement canPartition method for subset sum problem

---
+++ 416.java
@@ -0,0 +1,39 @@
+class Solution {
+    public boolean canPartition(int[] nums) {
+        int sum = 0;
+        for (int x : nums) {
+            sum += x;
+        }
+        
+        if (sum % 2 != 0) {
+            return false;
+        }
+        
+        int target = sum / 2;
+        
+        Boolean[][] dp = new Boolean[nums.length][target + 1];
+        
+        return func(nums.length - 1, target, nums, dp);
+    } 
+
+    public boolean func(int i, int target, int[] nums, Boolean[][] dp) {
+        if (target == 0) {
+            return true;
+        }
+        if (i == 0) {
+            return nums[0] == target;
+        }
+        if (dp[i][target] != null) {
+            return dp[i][target]; 
+        }
+        
+        boolean nottake = func(i - 1, target, nums, dp);
+        boolean take = false;
+        
+        if (nums[i] <= target) {
+            take = func(i - 1, target - nums[i], nums, dp);
+        }
+        
+        return dp[i][target] = take || nottake;
+    }
+}
