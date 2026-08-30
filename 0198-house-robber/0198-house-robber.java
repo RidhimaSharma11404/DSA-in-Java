e632aef1e0aa7f56837c58e83cd2a21c5df9dbc8
Ridhima Sharma <sharma.ridhima.2310@gmail.com>
2026-08-30 10:51:16 +0530

    Time: 0 ms (100%), Space: 42.8 MB (56.84%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -7,7 +7,7 @@ class Solution {
         if(n==1){
             return nums[0];
         }
-        int []dp=new  int[n];
+        int []dp=new int[n];
         dp[0]=nums[0];
         dp[1]=Math.max(nums[0],nums[1]);
         for(int i=2;i<n;i++){
