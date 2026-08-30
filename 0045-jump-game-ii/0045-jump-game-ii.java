5b2b4de25d7a2938c766ac4c446193b6a5470131
Ridhima Sharma <sharma.ridhima.2310@gmail.com>
2026-08-30 21:55:28 +0530

    Time: 1 ms (99.79%), Space: 47.2 MB (76.49%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.java
+++ 0045-jump-game-ii/0045-jump-game-ii.java
@@ -1,8 +1,8 @@
 class Solution {
     public int jump(int[] nums) {
         int jump=0;
-        int end=0;
         int far=0;
+        int end=0;
         for(int i=0;i<nums.length-1;i++){
             far=Math.max(far,i+nums[i]);
             if(i==end){
@@ -11,6 +11,7 @@ class Solution {
             }
         }
         return jump;
+
         
     }
 }
