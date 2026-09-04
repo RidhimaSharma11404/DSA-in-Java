db6078be4c8e32f10f8d20397c2ac1c513d7d0fa
Ridhima Sharma <sharma.ridhima.2310@gmail.com>
2026-09-05 01:14:13 +0530

    Time: 3 ms (24.62%), Space: 46.5 MB (50.28%) - LeetHub

---
+++ 0695-max-area-of-island/0695-max-area-of-island.java
@@ -0,0 +1,60 @@
+class Solution {
+    public int maxAreaOfIsland(int[][] grid) {
+
+        int n = grid.length;
+        int m = grid[0].length;
+
+        int maxArea = 0;
+
+        int[][] dir = {
+            {1,0},
+            {-1,0},
+            {0,1},
+            {0,-1}
+        };
+
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < m; j++) {
+
+                if(grid[i][j] == 1) {
+
+                    int area = 0;
+
+                    Queue<int[]> q = new LinkedList<>();
+                    q.add(new int[]{i,j});
+
+                    grid[i][j] = 0;
+
+                    while(!q.isEmpty()) {
+
+                        int[] curr = q.poll();
+
+                        int r = curr[0];
+                        int c = curr[1];
+
+                        area++;
+
+                        for(int[] d : dir) {
+
+                            int nr = r + d[0];
+                            int nc = c + d[1];
+
+                            if(nr >= 0 && nr < n &&
+                               nc >= 0 && nc < m &&
+                               grid[nr][nc] == 1) {
+
+                                grid[nr][nc] = 0;
+
+                                q.add(new int[]{nr,nc});
+                            }
+                        }
+                    }
+
+                    maxArea = Math.max(maxArea, area);
+                }
+            }
+        }
+
+        return maxArea;
+    }
+}
