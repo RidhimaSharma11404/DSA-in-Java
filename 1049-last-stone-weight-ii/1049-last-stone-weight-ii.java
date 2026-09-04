a861eb7e3ea56d20a29bef95d92c2597e0123ab2
Ridhima Sharma <sharma.ridhima.2310@gmail.com>
2026-09-04 18:27:20 +0530

    Time: 3 ms (56.02%), Space: 42.7 MB (90.79%) - LeetHub

---
+++ 1049-last-stone-weight-ii/1049-last-stone-weight-ii.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int lastStoneWeightII(int[] stones) {
+
+        int sum = 0;
+
+        for (int x : stones) {
+            sum += x;
+        }
+
+        int target = sum / 2;
+
+        int[] dp = new int[target + 1];
+
+        for (int stone : stones) {
+
+            for (int j = target; j >= stone; j--) {
+
+                dp[j] = Math.max(dp[j],
+                                 dp[j - stone] + stone);
+            }
+        }
+
+        return sum - 2 * dp[target];
+    }
+}
