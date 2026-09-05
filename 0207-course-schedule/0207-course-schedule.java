edc98fa43da9cb55fa741bc228b8e8260055499b
Ridhima Sharma <sharma.ridhima.2310@gmail.com>
2026-09-05 19:29:34 +0530

    Time: 5 ms (74.79%), Space: 47.3 MB (32.87%) - LeetHub

---
+++ 0207-course-schedule/0207-course-schedule.java
@@ -0,0 +1,57 @@
+class Solution {
+    public boolean canFinish(int V, int[][] edges) {
+     
+        ArrayList<Integer>[] graph = new ArrayList[V];
+        for(int i = 0; i < V; i++) {
+            graph[i] = new ArrayList<>();
+        }
+
+        // 2. Build graph + indegree
+        int[] indegree = new int[V];
+
+        for(int[] edge : edges) {
+
+            int u = edge[0];
+            int v = edge[1];
+
+            graph[u].add(v);
+            indegree[v]++;
+        }
+
+        // 3. Put indegree 0 nodes in queue
+        Queue<Integer> q = new LinkedList<>();
+
+        for(int i = 0; i < V; i++) {
+            if(indegree[i] == 0) {
+                q.add(i);
+            }
+        }
+
+        // 4. BFS
+        int[] ans = new int[V];
+        int index = 0;
+
+        while(!q.isEmpty()) {
+
+            int node = q.poll();
+
+            ans[index++] = node;
+
+            for(int neighbour : graph[node]) {
+
+                indegree[neighbour]--;
+
+                if(indegree[neighbour] == 0) {
+                    q.add(neighbour);
+                }
+            }
+        }
+
+        // 5. Cycle check
+        if(index != V) {
+            return false;
+        }
+
+        return true;
+    }
+}
