3cf292f9136f13b4ef99c3ddef383c87df2dbc15
Ridhima Sharma <sharma.ridhima.2310@gmail.com>
2026-09-06 20:16:53 +0530

    Time: 66 ms (29.56%), Space: 47.9 MB (37.67%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
@@ -3,23 +3,17 @@ class Solution {
         if(s.length()==0){
             return 0;
         }
-        int []freq=new int[128];
-        int left=0;int ans=0;
+        int ans=0;
+        int left=0;
+        HashSet<Character>set=new HashSet<>();
         for(int right=0;right<s.length();right++){
-            freq[s.charAt(right)]++;
-            while(freq[s.charAt(right)]>1){
-                freq[s.charAt(left)]--;
+            while(set.contains(s.charAt(right))){
+                set.remove(s.charAt(left));
                 left++;
             }
+            set.add(s.charAt(right));
             ans=Math.max(ans,right-left+1);
-
-            
         }
         return ans;
-        
     }
 }
-
-// Synced seamlessly with LeetHub Pro
-// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
-// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
