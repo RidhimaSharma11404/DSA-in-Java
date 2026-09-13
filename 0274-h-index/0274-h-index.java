23d56470d76b349a2f59c0b87db35942f72af30d
Ridhima Sharma <sharma.ridhima.2310@gmail.com>
2026-09-13 13:08:36 +0530

    Time: 5 ms (72.45%), Space: 43.5 MB (41.82%) - LeetHub

---
+++ 0274-h-index/0274-h-index.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int hIndex(int[] citations) {
+        Arrays.sort(citations);
+        int n=citations.length;
+        for(int i=0;i<citations.length;i++){
+            if(n-i<=citations[i]){
+                return n-i;
+            }
+        }
+        return 0;
+        
+
+        
+    }
+}
