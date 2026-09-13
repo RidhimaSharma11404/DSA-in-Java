215eeb848bf97f50910ad054092ff2acc2903a66
Ridhima Sharma <sharma.ridhima.2310@gmail.com>
2026-09-13 13:24:02 +0530

    Time: 2 ms (89.21%), Space: 47.7 MB (80.38%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -1,13 +1,19 @@
 class Solution {
     public boolean canJump(int[] nums) {
-        
-        int maxjumps=0;
-        for(int i=0;i<nums.length;i++){
-            if(i>maxjumps){
+        int maxjumps = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (i > maxjumps) {
                 return false;
             }
-            maxjumps=Math.max(maxjumps,i+nums[i]);
+
+            maxjumps = Math.max(maxjumps, i + nums[i]);
+
+            if (maxjumps >= nums.length - 1) {
+                return true;
+            }
         }
+
         return true;
     }
 }
