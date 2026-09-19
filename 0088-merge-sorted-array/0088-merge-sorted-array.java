0cdd1d29648df86ecb4f178791a30549e3248341
Ridhima Sharma <sharma.ridhima.2310@gmail.com>
2026-09-19 21:40:08 +0530

    Time: 0 ms (100%), Space: 43.9 MB (47.2%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.java
+++ 0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -12,13 +12,12 @@ class Solution {
                 j--;
             }
             k--;
-
         }
         while(j>=0){
             nums1[k]=nums2[j];
             j--;
             k--;
+
         }
-        
     }
 }
