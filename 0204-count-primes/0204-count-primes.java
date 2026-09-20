5c6188cf54c962f4761a72c8d16e9b1549989e45
Ridhima Sharma <sharma.ridhima.2310@gmail.com>
2026-09-20 10:49:49 +0530

    Time: 784 ms (20.13%), Space: 79.8 MB (25.98%) - LeetHub

--- 0204-count-primes/0204-count-primes.java
+++ 0204-count-primes/0204-count-primes.java
@@ -7,23 +7,19 @@ class Solution {
         for(int i=2;i<n;i++){
             prime[i]=true;
         }
-
         for(int i=2;i*i<n;i++){
             if(prime[i]){
                 for(int j=i*i;j<n;j+=i){
                     prime[j]=false;
                 }
             }
-
         }
         int count=0;
-        for(int i=2;i<n;i++){
+        for(int i=0;i<n;i++){
             if(prime[i]){
                 count++;
             }
-
         }
         return count;
-        
     }
 }
